01be8df641f534c7162a70e81a3b915dc5fe27fd
Marwam <marwamprog.js@gmail.com>
2023-03-27 20:55:15 -0300

    Seção 14: Mídias Aula 128 - 131: Utilizando ABAS. (Concluído)

--- app/src/main/java/com/cursoandroid/cursoandroidudemy/MainActivity.java
+++ app/src/main/java/com/cursoandroid/cursoandroidudemy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cursoandroid.cursoandroidudemy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,23 +11,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
 
+import com.cursoandroid.cursoandroidudemy.fragment.EmAltaFragment;
+import com.cursoandroid.cursoandroidudemy.fragment.HomeFragment;
+import com.cursoandroid.cursoandroidudemy.fragment.InscricoesFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 public class MainActivity extends AppCompatActivity {
 
-  
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       
+       smartTabLayout = findViewById(R.id.viewPagerTab);
+       viewPager = findViewById(R.id.viewPager);
+
+       getSupportActionBar().setElevation(10);
+
+       //Configurar adapter para abas
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Home", HomeFragment.class)
+                        .add("Inscrições", InscricoesFragment.class)
+                        .add("Em Alta", EmAltaFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter( adapter );
+        smartTabLayout.setViewPager( viewPager );
 
     }
-
-    public void abrirVideo(View view) {
-        startActivity( new Intent(this, PlayerActivity.class) );
-    }
-
-
 
 }
