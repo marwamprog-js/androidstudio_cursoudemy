c0f25b1b55fbc7550ef81668a1c063676198fecc
Marwam <marwamprog.js@gmail.com>
2023-03-28 11:22:42 -0300

    Seção 14: Mídias Aula 132 - 135: App Aprenda Inglês. (Concluído)

--- app/src/main/java/com/cursoandroid/cursoandroidudemy/MainActivity.java
+++ app/src/main/java/com/cursoandroid/cursoandroidudemy/MainActivity.java
@@ -3,19 +3,12 @@ package com.cursoandroid.cursoandroidudemy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Context;
-import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.SeekBar;
 
-import com.cursoandroid.cursoandroidudemy.fragment.EmAltaFragment;
-import com.cursoandroid.cursoandroidudemy.fragment.HomeFragment;
-import com.cursoandroid.cursoandroidudemy.fragment.InscricoesFragment;
+import com.cursoandroid.cursoandroidudemy.fragment.BichosFragment;
+import com.cursoandroid.cursoandroidudemy.fragment.NumerosFragment;
+import com.cursoandroid.cursoandroidudemy.fragment.VogaisFragment;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
-import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
@@ -24,28 +17,29 @@ public class MainActivity extends AppCompatActivity {
     private SmartTabLayout smartTabLayout;
     private ViewPager viewPager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       smartTabLayout = findViewById(R.id.viewPagerTab);
-       viewPager = findViewById(R.id.viewPager);
+        getSupportActionBar().setElevation(0);
 
-       getSupportActionBar().setElevation(10);
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
 
-       //Configurar adapter para abas
         FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
                 getSupportFragmentManager(),
                 FragmentPagerItems.with(this)
-                        .add("Home", HomeFragment.class)
-                        .add("Inscrições", InscricoesFragment.class)
-                        .add("Em Alta", EmAltaFragment.class)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Números", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
                         .create()
         );
 
         viewPager.setAdapter( adapter );
         smartTabLayout.setViewPager( viewPager );
+
 
     }
 
